f1505f783b9c069addd0955cd4d1b1e7f6670342
Bianca Ion <ibianca7469@gmail.com>
2022-10-21 15:47:37 +0300

    am pus css in styles

---
+++ scripts/Sidebar.js
@@ -0,0 +1,13 @@
+//Script to open/close the sidebar
+function w3_open() {
+  document.getElementById("mySidebar").style.display = "block";
+}
+
+function w3_openAbout() {
+    document.getElementById("about").style.display = "block";
+  }
+
+function w3_close() {
+  document.getElementById("mySidebar").style.display = "none";
+}
+
