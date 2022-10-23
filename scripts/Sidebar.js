571bf5fd51f32c962fd0d6dfc26680980dfb72e4
Bianca Ion <ibianca7469@gmail.com>
2022-10-23 18:28:06 +0300

    Proiect html

--- scripts/Sidebar.js
+++ scripts/Sidebar.js
@@ -4,10 +4,9 @@ function w3_open() {
 }
 
 function w3_openAbout() {
-    document.getElementById("about").style.display = "block";
-  }
+  document.getElementById("about").style.display = "block";
+}
 
 function w3_close() {
   document.getElementById("mySidebar").style.display = "none";
 }
-
